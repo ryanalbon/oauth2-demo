2d2825429ff2f7bfc239e108e224623bd6661ae5
Ryan Albon <ryanalbon@gmail.com>
2019-10-12 13:58:39 +0200

    chore: Implementing 'join' and 'login' provider endpoints.

--- provider/src/app.js
+++ provider/src/app.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
 const expressPinoLogger = require('express-pino-logger');
+const joi = require('@hapi/joi');
+const jwt = require('jsonwebtoken');
 const mongodb = require('mongodb');
 
 const logger = require('./logger');
@@ -38,20 +41,39 @@ app.get('/authorize', (req, res) => res.sendStatus(501));
  */
 app.post('/token', (req, res) => res.sendStatus(501));
 
-function viewUser({ _id, password, ...rest }) {
+function viewUser({ _id, passwordHash, ...rest }) {
   return {
     id: String(_id),
     ...rest,
   };
 }
 
+const userSchema = joi.object().unknown(false).keys({
+  email: joi.string().email().trim().lowercase().required(),
+  password: joi.string().required(),
+});
+
 app.post('/join', express.json(), async (req, res) => {
   const db = await getDBConnection();
 
-  const user = {
-    ...req.body,
-    createdAt: new Date(),
-  };
+  const { error, value } = userSchema.validate(req.body);
+  const { password, ...user } = value;
+
+  if (error) {
+    return res.status(400)
+      .json({ error })
+      .end();
+  }
+
+  const emailTaken = await db.db('oauth2_provider_db').collection('users').findOne({ email: user.email })
+
+  if (emailTaken) {
+    return res.status(400).end();
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  Object.assign(user, { createdAt: Date.now(), passwordHash });
 
   await db.db('oauth2_provider_db').collection('users').insertOne(user);
 
@@ -60,6 +82,47 @@ app.post('/join', express.json(), async (req, res) => {
     .end();
 });
 
-app.post('/login', (req, res) => res.sendStatus(501));
+const loginSchema = joi.object().unknown(false).keys({
+  email: joi.string().trim().lowercase().email().required(),
+  password: joi.string().required(),
+});
+
+app.post('/login', express.json(), async (req, res) => {
+  const { error, value } = loginSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ error }).end();
+  }
+
+  const db = await getDBConnection();
+  const criteria = { email: value.email };
+  const user = await db.db('oauth2_provider_db').collection('users').findOne(criteria);
+
+  if (!user) {
+    return res.status(401).end();
+  }
+
+  const passwordIsGood = await bcrypt.compare(value.password, user.passwordHash);
+
+  if (!passwordIsGood) {
+    return res.status(401).end();
+  }
+
+  const tokenPayload = {
+    exp: Date.now() + 1000 * 60,
+    iat: Date.now(),
+    iss: 'oauth2-provider.localhost',
+    aud: 'oauth2-provider.localhost',
+    sub: String(user._id),
+  };
+
+  const token = jwt.sign(tokenPayload, 'secret');
+
+  return res.status(200)
+    .set('Cache-Control', 'no-cache, no-store')
+    .set('Pragma', 'no-cache')
+    .json({ access_token: token, token_type: 'Bearer' })
+    .end();
+});
 
 module.exports = app;
